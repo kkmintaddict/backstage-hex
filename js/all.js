c4bacf9374de6a7e1e391a0dae805302792c06fe
kkmintaddict <146964446+kkmintaddict@users.noreply.github.com>
2023-11-23 15:51:10 +0800

    整理

--- js/all.js
+++ js/all.js
@@ -1,8 +1,5 @@
 
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl)
-})
+
 
 const toggleMenuBtn = document.querySelector('#toggle-btn');
 const body = document.querySelector('body');
